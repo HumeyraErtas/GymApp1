b50c316f5a385f552613e76d7c03bfe3bed8502c
Alperen10 <alperenakgun1905@outlook.com>
2022-04-23 13:38:44 +0300

    splash screen added

--- app/src/main/java/com/alperen/gymapp/MainActivity.kt
+++ app/src/main/java/com/alperen/gymapp/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Toast.makeText(this,"Welcome",Toast.LENGTH_SHORT).show()
 
         val btnAlert = findViewById<Button>(R.id.btnAlert)
 
