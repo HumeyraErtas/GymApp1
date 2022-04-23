85beced01440dea29a1f586051bbb2267e945c3f
Alperen10 <alperenakgun1905@outlook.com>
2022-04-23 12:25:36 +0300

    notification create completed

---
+++ app/src/main/java/com/alperen/gymapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.alperen.gymapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnAlert = findViewById<Button>(R.id.btnAlert)
+
+        btnAlert.setOnClickListener {
+            val intent = Intent(this,NotificationActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
